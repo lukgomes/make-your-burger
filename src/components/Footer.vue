30e2e61c23ac8d7b69426132410f8ad331f4eae6
lucas gomes <lukmck@gmail.com>
2022-09-03 21:46:15 -0300

    Criação da barra de navegação e footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <p>Make Your Burger &copy; 2022</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
+
+<style scoped>
+  .footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #fcba03;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
